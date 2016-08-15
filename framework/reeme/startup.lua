419dfe4ad827562720e26bb48a38b900bc351ab7
mobilesolution2016 <mobilesolution@qq.com>
2016-08-15 13:17:57 +0800

    。

--- framework/reeme/startup.lua
+++ framework/reeme/startup.lua
@@ -1,16 +1,37 @@
-local appRootDir = nil
+local appRootDir = ngx.var.APP_ROOT
 if not appRootDir then
-	local appRootDir = ngx.var.APP_ROOT
-	if not appRootDir then
-		ngx.say("APP_ROOT is not definde in app nginx conf")
-		ngx.eof()
-	end
-	
+	ngx.say("APP_ROOT is not definde in app nginx conf")
+	ngx.eof()
+end
+
+local currentAppPath = appRootDir .. '/?.lua'
+if not string.find(package.path, currentAppPath) then
 	if package.path and #package.path > 0 then
-		package.path = package.path .. ';' .. appRootDir .. '/?.lua'
+		package.path = package.path .. ';' .. currentAppPath
 	else
-		package.path = appRootDir .. '/?.lua'
+		package.path = appRootDir .. currentAppPath
+	end
+end
+
+local oldRequire = require
+
+local ffi = require("ffi")
+function require(name)
+	local appName = appRootDir .. "/" .. name
+	
+    if package.loaded[appName] then 
+		return package.loaded[appName]
 	end
 
-	require("public.index")()
+	local searchPrefix = ffi.abi('win') and "?.lua;?/init.lua" or "/?.lua;/?/init.lua"
+	local moduleName = nil
+	if package.searchpath(appName, "?.lua;?/init.lua") ~= nil then
+        moduleName = appName
+    else
+        moduleName = name
+    end
+	
+    return oldRequire(moduleName)
 end
+
+require("public.index")()
