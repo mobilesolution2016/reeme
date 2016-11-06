4b4d9711dffe2708bdb6990ced7ebdbe499c3301
mobilesolution2016 <mobilesolution@qq.com>
2016-11-07 01:08:28 +0800

    。

--- c/reemext/preheader.h
+++ c/reemext/preheader.h
@@ -15,6 +15,7 @@
 #include <list>
 
 #ifdef _WINDOWS
+#	define WIN32_LEAN_AND_MEAN             // 从 Windows 头中排除极少使用的资料
 #	include <windows.h>
 #	include <tchar.h>
 #	include <boost/unordered_map.hpp>
